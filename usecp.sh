e009b441e39f88722602449aafcc301d7873555c
Popa Ioan Alexandru <alexioanpopa11@gmail.com>
2021-04-11 00:54:01 +0300

    Tweaked ubuntu script to ensure the proper pwd; added polybar to arch linux bspwm packages list

--- usecp.sh
+++ usecp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd $(dirname $(readlink -f "$0") )
 sudo apt install apt-transport-https curl
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
