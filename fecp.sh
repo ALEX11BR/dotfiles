bc80b747879f1cb6c6c98a92bec2dbeb5b242296
Popa Ioan Alexandru <alexioanpopa11@gmail.com>
2021-04-24 20:14:36 +0300

    Small tweaks to fedora script

--- fecp.sh
+++ fecp.sh
@@ -15,15 +15,16 @@ pmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm htt
 sudo rpm -ivh http://rpm.livna.org/livna-release.rpm
 sudo dnf update
 sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda vdpauinfo libva-vdpau-driver libva-utils
-sudo dnf install -y google-noto-\* vlc gstreamer1-plugins-{bad-\*,good-\*,ugly-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav lame\* libdvdcss
+sudo dnf install -y flatpak google-noto-\* vlc gstreamer1-plugins-{bad-\*,good-\*,ugly-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav lame\* libdvdcss
 sudo dnf install -y zsh zsh-syntax-highlighting zsh-autosuggestions
-sudo dnf install -y network-manager-applet neovim git
+sudo dnf install -y neovim git
 sudo dnf install -y alacritty
 echo "max_parallel_downloads=10" | sudo tee -a /etc/dnf/dnf.conf >/dev/null
 echo "fastestmirror=1" | sudo tee -a /etc/dnf/dnf.conf >/dev/null
-bash cpconf -v "/etc/vimrc.local" -f "50-families.conf" -xp -n -z "/etc/zshrc" -zs "s;/zsh/plugins;;" -c i3 -c alacritty -c awesome -c qtile -c bspwm -c sxhkd -c termite -c polybar -t alacritty
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+bash cpconf -v "/etc/vimrc.local" -f "50-families.conf" -n -z "/etc/zshrc" -zs "s;/zsh/plugins;;" -c i3 -c alacritty -c awesome -c qtile -c bspwm -c sxhkd -c termite -c polybar -t alacritty
 if [ "$1" = "bspwm" ]; then
-	sudo dnf install bspwm sxhkd polybar picom udiskie parcellite notification-daemon maim file-roller numlockx network-manager-applet picom udiskie dnfdragora-updater
+	sudo dnf install bspwm sxhkd polybar picom udiskie parcellite blueman notification-daemon maim file-roller numlockx network-manager-applet picom udiskie dnfdragora-updater
+sed "s/pamac-tray/dnfdragora-updater/" xprofile | sudo tee /etc/xprofile >/dev/null
 fi
-sed "s/pamac-tray/dnfdragora-updater/" /etc/xprofile | sudo tee /etc/xprofile >/dev/null
 
