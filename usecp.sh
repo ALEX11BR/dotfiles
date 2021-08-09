0f73433719284923a0ba5ccfd8f6861e509e1ef5
Popa Ioan Alexandru <alexioanpopa11@gmail.com>
2021-08-10 01:46:57 +0300

    Remember to install gtk theme

--- usecp.sh
+++ usecp.sh
@@ -15,7 +15,7 @@ rm packages.microsoft.gpg
 
 sudo apt-get install zsh zsh-autosuggestions zsh-syntax-highlighting git neovim fonts-noto vlc vlc-plugin-fluidsynth libdvdcss2 gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav
 
-bash cpconf -v "/etc/vim/vimrc.local" -ps "s/wheel/sudo/g" -zs "s;/zsh/plugins;;" -n -f "50-families.conf"
+bash cpconf -v "/etc/vim/vimrc.local" -ps "s/wheel/sudo/g" -zs "s;/zsh/plugins;;" -ct oomox-dracula -n -f "50-families.conf"
 echo '[Do anything you want]
 Identity=unix-group:sudo
 Action=*
