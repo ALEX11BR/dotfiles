5b48abce4c7f4a2ae2657b9e1b5dd80717aac413
Popa Ioan Alexandru <alexioanpopa11@gmail.com>
2023-04-01 01:37:14 +0300

    added dejavu fonts so that arabic text doesn't look weird on arch

--- vecp.sh
+++ vecp.sh
@@ -6,7 +6,7 @@ if [ "$EUID" = 0 ]; then
 fi
 sudo xbps-install -Suf
 sudo xbps-install -Suf void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree
-sudo xbps-install -Suf NetworkManager sxhkd xclip playerctl numlockx htop wget zsh zsh-completions zsh-syntax-highlighting zsh-autosuggestions lightdm lightdm-gtk3-greeter qt5-styleplugins gnome-themes-extra breeze-icons mesa-dri xss-lock xsecurelock network-manager-applet alacritty gmrun qbittorrent upower gvim git unrar p7zip xdg-user-dirs-gtk gvfs-mtp parcellite picom octoxbps gst-libav gst-plugins-bad1 gst-plugins-good1 gst-plugins-ugly1 yt-dlp cups hplip ntfs-3g nvidia nvidia-libs-32bit intel-video-accel xorg-minimal elogind pulseaudio alsa-plugins-pulseaudio bluez blueman acpilight noto-fonts-ttf noto-fonts-emoji noto-fonts-cjk libreoffice chromium
+sudo xbps-install -Suf NetworkManager sxhkd xclip playerctl numlockx htop wget zsh zsh-completions zsh-syntax-highlighting zsh-autosuggestions lightdm lightdm-gtk3-greeter qt5-styleplugins gnome-themes-extra breeze-icons mesa-dri xss-lock xsecurelock network-manager-applet alacritty gmrun qbittorrent upower gvim git unrar p7zip xdg-user-dirs-gtk gvfs-mtp parcellite picom octoxbps gst-libav gst-plugins-bad1 gst-plugins-good1 gst-plugins-ugly1 yt-dlp cups hplip ntfs-3g nvidia nvidia-libs-32bit intel-video-accel xorg-minimal elogind pulseaudio alsa-plugins-pulseaudio bluez blueman acpilight dejavu-fonts-ttf noto-fonts-ttf noto-fonts-emoji noto-fonts-cjk libreoffice chromium
 if [ "$1" = "bspwm" ]; then
 	sudo xbps-install -Suf bspwm polybar rofi udiskie xfce4-notifyd maim xsettingsd gnome-keyring file-roller pcmanfm-qt
 fi
