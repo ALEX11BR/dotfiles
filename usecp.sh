10dce7d470c791afca279e5fee1d2383ef768b0c
Popa Ioan Alexandru <alexioanpopa11@gmail.com>
2025-01-20 09:00:04 +0200

    install unrar for ubuntu and fedora

--- usecp.sh
+++ usecp.sh
@@ -17,7 +17,7 @@ sudo apt-get update
 sudo apt-get install code
 rm packages.microsoft.gpg
 
-sudo apt-get install zsh zsh-autosuggestions zsh-syntax-highlighting git neovim yt-dlp fonts-noto sxhkd numlockx xclip htop qalculate-gtk vlc vlc-plugin-fluidsynth libdvdcss2 gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav python3-usb python-is-python3
+sudo apt-get install zsh zsh-autosuggestions zsh-syntax-highlighting git neovim yt-dlp fonts-noto sxhkd numlockx xclip htop qalculate-gtk vlc vlc-plugin-fluidsynth unrar libdvdcss2 gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav python3-usb python-is-python3
 sudo apt-get install latexmk texlive-base texlive-bibtex-extra texlive-binaries texlive-fonts-extra texlive-fonts-recommended texlive-font-utils texlive-formats-extra texlive-games texlive-humanities texlive-latex-extra texlive-latex-recommended texlive-luatex texlive-science texlive-metapost texlive-music texlive-pictures texlive-plain-generic texlive-pstricks texlive-publishers texlive-xetex texlive-lang-english texlive-lang-european
 
 if [ "$1" = "bspwm" ]; then
