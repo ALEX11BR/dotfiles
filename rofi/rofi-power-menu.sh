94698a15c168d136de9638f29c8ffc7af130edff
Popa Ioan Alexandru <alexioanpopa11@gmail.com>
2025-04-13 01:51:15 +0300

    tweaks in hyprland

--- rofi/rofi-power-menu.sh
+++ rofi/rofi-power-menu.sh
@@ -41,7 +41,7 @@ icons[cancel]="\u00d7"
 declare -A actions
 actions[lockscreen]="loginctl lock-session ${XDG_SESSION_ID-}"
 #actions[switchuser]="???"
-actions[logout]="loginctl terminate-session ${XDG_SESSION_ID-}"
+actions[logout]="loginctl kill-session ${XDG_SESSION_ID-}"
 actions[suspend]="systemctl suspend"
 actions[hibernate]="systemctl hibernate"
 actions[reboot]="systemctl reboot"
